5944d5fd41cc78a6f53a8ba7aea94199122c5165
patxol <pol@lemonbox.fr>
2021-01-16 21:25:47 +0100

    Adding some comments

--- routes/raclette.js
+++ routes/raclette.js
@@ -3,13 +3,15 @@ const raclette = require('../models/raclette');
 const RacletteService = require("../services/raclette")
 
 module.exports = (app) => {
+    /* Create a raclette device, defining its owner
+        Returns the URL to get its status
+    */
     app.post("/create", async (req, res) => {
         const newRaclette = await RacletteService.create(req.body.name)
-        console.log(newRaclette)
         res.json("http://localhost:7374/status/"+newRaclette._id)
     })
+    //Get the status of a Raclette device
     app.get("/status/:id", async (req, res) => {
-        
         res.json(await RacletteService.status(req.params.id));
     })
     app.get("/on/:id", async (req, res) => {
