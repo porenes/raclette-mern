e425d18ccbceaf0b0d76150e6dd199ea88f43b39
patxol <pol@lemonbox.fr>
2021-01-17 13:45:10 +0100

    Setting up dotenv

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-const express = require("express")
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 
-// Define app
 const app = express();
+
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+// Dotenv configuration
+require("dotenv").config({path:'./config/.env'});
+// Database configuration
+require("./config/db.js")();
+
+// default route
 app.get("/", (req, res) => {
-    res.json("J'aime la raclette 🧀");
-})
-require("./config/db.js")()
+  res.json("J'aime la raclette 🧀");
+});
 
-const racletteRoute = require("./routes/racletteParty")
-app.use('/party', racletteRoute)
-const connoisseurRoute = require("./routes/connoisseur")
-app.use('/connoisseur', connoisseurRoute)
+const racletteRoute = require("./routes/racletteParty");
+app.use("/party", racletteRoute);
+const connoisseurRoute = require("./routes/connoisseur");
+app.use("/connoisseur", connoisseurRoute);
 
 //Defin and start server
-const port = 7374;
-app.listen(port, () => console.log(`🧀 Cheese ! I'm started on http://localhost:${port}`));
+const port = process.env.PORT;
+app.listen(port, () =>
+  console.log(`🧀 Cheese ! I'm started on http://localhost:${port}`)
+);
--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 module.exports = () => {
-
-    mongoose.connect('mongodb://localhost/raclette', {useNewUrlParser: true, useUnifiedTopology: true});
+    console.log("Trying to connect to Mongo on :"+process.env.MONGO_URI )
+    mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
