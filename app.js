a173bdb23b512b2c333ef4b7bf2d6511631a1ccb
patxol <pol@lemonbox.fr>
2021-01-27 22:31:15 +0100

    CORS policy fix

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ require("dotenv").config({ path: "./config/.env" });
 const corsOptions = {
   origin: process.env.CLIENT_URL,
   credentials: true,
-  allowedHeaders: ["sessionId", "Content-Type"],
-  exposedHeaders: ["sessionId"],
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
 };
