6205ebc1fae5a2c792f9d92a199ca74172e38c5d
patxol <pol@lemonbox.fr>
2021-02-02 22:45:34 +0100

    🐛 Save user even if auth is optional

--- middlewares/Auth.js
+++ middlewares/Auth.js
@@ -4,6 +4,7 @@ module.exports = {
   optional: (req, res, next) => {
     passport.authenticate("bearer", { session: false }, (err, user, info) => {
       req.authenticated = !!user;
+      req.user = user
       user
         ? console.log("Authenticated as ", user.email)
         : console.log("Anonymous");
